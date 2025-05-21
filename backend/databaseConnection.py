88212d2603d2c8e7e212d83858545fe3cf25ddd7
Aksel <aknea001@osloskolen.no>
2025-05-21 13:48:42 +0200

    satt opp database connection i backend

---
+++ backend/databaseConnection.py
@@ -0,0 +1,42 @@
+from mysql.connector import pooling, Error as mysqlError
+from dotenv import load_dotenv
+from os import getenv
+
+class Database():
+    def __init__(self):
+        load_dotenv()
+
+        self.dbConfig = {
+            "host": getenv("sqlhost"),
+            "user": getenv("sqluser"),
+            "password": getenv("sqlpasswd"),
+            "database": getenv("sqldb")
+        }
+
+        self.pool = pooling.MySQLConnectionPool (
+            pool_name = "mypool",
+            pool_size = 5,
+            **self.dbConfig
+        )
+    
+    def execute(self, query: str, *args, **kw: int):
+        values = tuple(args)
+        amount = kw.get("a", 1)
+
+        try:
+            connection = self.pool.get_connection()
+            cursor = connection.cursor(buffered=True)
+
+            cursor.execute(query, values)
+
+            if query.upper().startswith("SELECT"):
+                data = cursor.fetchone() if amount <= 1 else cursor.fetchall()
+
+                return data
+            
+            connection.commit()
+        except mysqlError as e:
+            return e
+        finally:
+            cursor.close()
+            connection.close()
